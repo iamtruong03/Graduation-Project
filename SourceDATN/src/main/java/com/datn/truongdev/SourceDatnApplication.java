ec08e39336b5b2e0a96231c090c93b12dfb6fb3c
truong minh <mtruongg203@gmail.com>
2025-03-31 08:56:39 +0700

    first commit

---
+++ SourceDATN/src/main/java/com/datn/truongdev/SourceDatnApplication.java
@@ -0,0 +1,13 @@
+package com.datn.truongdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceDatnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceDatnApplication.class, args);
+	}
+
+}
---
+++ SourceDATN/src/test/java/com/datn/truongdev/SourceDatnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datn.truongdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceDatnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

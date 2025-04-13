660c7548dbf8c373417e9fff03923e6176c75c96
truong minh <mtruongg203@gmail.com>
2025-04-13 18:19:58 +0700

    Remove node_modules from tracking

--- truongdev/src/main/java/com/dev/truongdev/service/IDepartmentService.java
+++
@@ -1,8 +0,0 @@
-package com.dev.truongdev.service;
-
-import com.dev.truongdev.entity.Department;
-import com.dev.truongdev.xdevbase.service.IXDevBaseService;
-
-public interface IDepartmentService extends IXDevBaseService<Department> {
-
-}
--- truongdev/src/main/java/com/dev/truongdev/service/IUserService.java
+++
@@ -1,8 +0,0 @@
-package com.dev.truongdev.service;
-
-import com.dev.truongdev.entity.User;
-import com.dev.truongdev.xdevbase.service.IXDevBaseService;
-
-public interface IUserService extends IXDevBaseService<User> {
-  User save(User user);
-}
